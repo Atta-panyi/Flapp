f123fffd47f35931d790813804ffbefc1ccfdeff
Marie <maryagbenyeku@gmail.com>
2024-04-25 14:59:30 +0200

    nothing

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 from flask import Flask 
+from markupsafe import escape
 app = Flask (__name__)
 
 @app.route('/')
-def Hello_world():
-    return 'Hello Marie'
+def Hello_world(name):
+    return f'hello, {escape(name)}'
+
+
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True, host='0.0.0.0', port=80) 
+
+
